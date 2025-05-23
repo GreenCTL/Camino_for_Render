11ab3e27cc7725be70ffacc6071f40b3abd4b272
GreenCTL <aaaaaa55589@gmail.com>
2025-05-24 06:01:27 +0800

    小修改

--- Camino_team1/React/vite-project/vite.config.js
+++ Camino_team1/React/vite-project/vite.config.js
@@ -1,17 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path' // ✅ 補上這行！
-import { fileURLToPath } from 'url'
-import path from 'path';
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: path.resolve(__dirname, 'src'), // 保持當前根目錄（Render 指向這層）
   base: './',
   plugins: [react()],
   build: {
-    outDir: path.resolve(__dirname, 'dist'),
+    outDir: 'dist',
   },
-})
+});
