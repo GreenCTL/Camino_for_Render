efb24cca9dba6b464f00ede54981218c365ac973
GreenCTL <aaaaaa55589@gmail.com>
2025-05-24 05:44:14 +0800

    修改路徑

--- Camino_team1/React/vite-project/vite.config.js
+++ Camino_team1/React/vite-project/vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root:'vite-project',
+  root: '.', // 保持當前根目錄（Render 指向這層）
   base: './',
   plugins: [react()],
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: resolve(__dirname, 'src/index.html')  // 👉 指定 index.html 路徑
+    }
   },
 })
