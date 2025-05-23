d6b4909da7e9546377f8b58fcc800a80adaa6eb7
GreenCTL <aaaaaa55589@gmail.com>
2025-05-24 01:14:35 +0800

    測試creat-payment功能是否正常

--- app.js
+++ app.js
@@ -100,9 +100,9 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-// 測試用
-app.get("/test", (req, res) => {
-  res.send("✅ Camino backend2 is running");
+//測試creat-payment功能
+router.get("/create-payment", (req, res) => {
+  res.send("(測試)請透過 POST 方式提交付款資料");
 });
 
 //port
