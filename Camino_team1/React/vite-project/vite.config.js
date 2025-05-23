edb9744f386c795b20ff85fb598ae5226d3d837c
GreenCTL <aaaaaa55589@gmail.com>
2025-05-24 05:53:48 +0800

    path

--- Camino_team1/React/vite-project/vite.config.js
+++ Camino_team1/React/vite-project/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path' // ✅ 補上這行！
 import { fileURLToPath } from 'url'
-import { path } from 'framer-motion/client'
+import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
