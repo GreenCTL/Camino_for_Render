7bebe3fe2e9fc5f02623dad0ac6cec3d555d4217
GreenCTL <aaaaaa55589@gmail.com>
2025-05-24 05:57:23 +0800

    33

--- Camino_team1/React/vite-project/vite.config.js
+++ Camino_team1/React/vite-project/vite.config.js
@@ -8,13 +8,10 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  root: '.', // 保持當前根目錄（Render 指向這層）
+  root: path.resolve(__dirname, 'src'), // 保持當前根目錄（Render 指向這層）
   base: './',
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: resolve(__dirname, 'src/index.html')  // 👉 指定 index.html 路徑
-    }
+    outDir: path.resolve(__dirname, 'dist'),
   },
 })
