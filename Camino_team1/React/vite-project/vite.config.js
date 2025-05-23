db37e423a8a8eb420cc4a646c4244b78f7362eda
GreenCTL <aaaaaa55589@gmail.com>
2025-05-24 05:48:33 +0800

    resolve

--- Camino_team1/React/vite-project/vite.config.js
+++ Camino_team1/React/vite-project/vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path' // ✅ 補上這行！
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   root: '.', // 保持當前根目錄（Render 指向這層）
